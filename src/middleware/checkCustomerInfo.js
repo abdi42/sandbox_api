7fad1b1304032254675f0cf7c22fa23ac1ce8c74
abdi42 <abdullahimahamed42@gmail.com>
2016-12-14 21:50:28 -0600

    changed callback

--- src/middleware/checkCustomerInfo.js
+++ src/middleware/checkCustomerInfo.js
@@ -2,20 +2,20 @@ module.exports = function(req,res,next){
   if(!req.body.plan){
     var err = new Error("subscription plan not provided");
     err.status = 400;
-    return callback(err);
+    return next(err);
   }
 
   if(!req.body.source){
     var err = new Error("customer source not provided");
     err.status = 400;
-    return callback(err)
+    return next(err)
   }
 
   if(!req.body.email){
     var err = new Error("customer email not provided")
     err.status = 400;
-    return callback(err);
+    return next(err);
   }
 
-  return callback();
+  return next();
 }
